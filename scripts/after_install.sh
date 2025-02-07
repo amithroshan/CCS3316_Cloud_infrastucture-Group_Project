ee0afcd579401a0e8b57d767d0f6fcd3e9acc6ad
amithroshan <amithroshan9@gmail.com>
2025-02-07 13:02:38 +0530

    last

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -10,3 +10,12 @@ echo "Restarting the application..."
 pm2 restart app || pm2 start app.js --name app
 
 echo "Deployment completed successfully."
+
+
+sudo mv public/index.html .
+sudo mv public/login.html .
+sudo mv public/signup.html .
+sudo mv public/style.css .
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
